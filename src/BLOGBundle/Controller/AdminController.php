af62850efa8ac91b1c07d65888d74f9f7eafc4cf
florenta <florent.alemany@gmail.com>
2017-04-11 11:21:09 +0200

    Ajout delete, création catégorie Autres si rien renseigné

--- src/BLOGBundle/Controller/AdminController.php
+++ src/BLOGBundle/Controller/AdminController.php
@@ -58,56 +58,105 @@ class AdminController extends Controller
 
 		if ($form->isSubmitted() && $form->isValid()) {
 
-				foreach($request->request->get('category') as $category)
-				{
-					if($category !== "") // Si catégorie est vide (y compris si select est laissé sur 'choisir une catégorie')
-					{
-						if($keyword) 
-						{			
-							$i=0; $j=0; $endLoop = false; $loopIndex=0; $loopLength=count($keyword);
-							foreach($keyword as $key){
+		        if($request->request->get('category'))
+		        {
+                    foreach ($request->request->get('category') as $category)
+                    {
+                        if ($category !== "") // Si catégorie est vide (y compris si select est laissé sur 'choisir une catégorie')
+                        {
+                            if ($keyword) {
+                                $i = 0;
+                                $j = 0;
+                                $endLoop = false;
+                                $loopIndex = 0;
+                                $loopLength = count($keyword);
+                                foreach ($keyword as $key) {
+                                    // Si le nouveau mot-clef n'existe pas en bdd, alors on incrémente le compteur pour avoir une trace
+                                    if ($category !== $key->getCategory()) {
+                                        $j++;
+                                    }
+                                    // Si le mot-clef renseigné existe déjà, alors on l'associe simplement à l'article créé
+                                    if ($category == $key->getCategory()) {
+                                        $key->addArticle($article);
+                                        $article->addCategory($key);
+                                        $i++;
+                                    }
 
-								// Si le nouveau mot-clef n'existe pas en bdd, alors on incrémente le compteur pour avoir une trace
-								if($category !== $key->getCategory()) 
-								
-								{
-									$j++;
-								}
-								
-								// Si le mot-clef renseigné existe déjà, alors on l'associe simplement à l'article créé
-								if($category == $key->getCategory())
-								{					
-									$key->addArticle($article);
-									$article->addCategory($key);
-									$i++;
-								}
-															
-								// Si le mot-clef renseigné n'existe pas encore, alors on l'ajoute en bdd et on fait l'association
+                                    // Si le mot-clef renseigné n'existe pas encore, alors on l'ajoute en bdd et on fait l'association
 
-								$loopIndex++; // On ajoute un tour de boucle
-								
-								if($loopLength == $loopIndex){
-									$endLoop = true; // Si on a parcouru, alors fin des boucles
-								}
-							}
-							
-							// Puisque j est supérieur à 0, alors on doit créer une catégorie.
-							if($j>0 AND $i==0 AND $endLoop == true){
-								$categ = new Category();
-								$categ->setCategory($category);
-								$categ->addArticle($article);
-								$article->addCategory($categ);
-							}
-						}
-						else // Si la base est vide, je crée ma première catégorie
-						{
-							$categ = new Category();
-							$categ->setCategory($category);
-							$categ->addArticle($article);
-							$article->addCategory($categ);
-						}
-					}
-				}
+                                    $loopIndex++; // On ajoute un tour de boucle
+
+                                    if ($loopLength == $loopIndex) {
+                                        $endLoop = true; // Si on a parcouru, alors fin des boucles
+                                    }
+                                }
+                                // Puisque j est supérieur à 0, alors on doit créer une catégorie.
+                                if ($j > 0 AND $i == 0 AND $endLoop == true) {
+                                    $categ = new Category();
+                                    $categ->setCategory($category);
+                                    $categ->addArticle($article);
+                                    $article->addCategory($categ);
+                                }
+                            } else // Si la base est vide, je crée ma première catégorie
+                            {
+                                $categ = new Category();
+                                $categ->setCategory($category);
+                                $categ->addArticle($article);
+                                $article->addCategory($categ);
+                            }
+                        } else { // On gère le set automatique à "Autres" si aucune catégorie indiquée
+                            // Deux cas de figure ->
+                            // La catégorie "Autres" n'existe pas en bdd. Il faut la créer.
+                            // Elle existe en bdd -> on associe l'article à "Autres"
+                            $i = 0;
+                            foreach ($keyword as $key) {
+
+                                if ($key->getCategory() == "Autres") {
+                                    $key->addArticle($article);
+                                    $article->addCategory($key);
+                                    $i++;
+                                }
+
+                                if($i==0){
+                                    $categ = new Category();
+                                    $categ->setCategory("Autres");
+                                    $categ->addArticle($article);
+                                    $article->addCategory($categ);
+                                }
+                            }
+                        }
+                    }
+                }
+				else
+                {
+                    $i=0;
+                    if($keyword)
+                    {
+                        echo "bonjour";
+                        die();
+                        foreach ($keyword as $key)
+                        {
+                            $key->addArticle($article);
+                            $article->addCategory($key);
+                            $i++;
+                        }
+
+                        if($i == 0)
+                        {
+                            $categ = new Category();
+                            $categ->setCategory("Autres");
+                            $categ->addArticle($article);
+                            $article->addCategory($categ);
+                        }
+                    }
+                    else
+                    {
+                        $categ = new Category();
+                        $categ->setCategory("Autres");
+                        $categ->addArticle($article);
+                        $article->addCategory($categ);
+                    }
+                }
 
 			// On vérifie qu'il y a au moins une image envoyée
 			if(!empty($request->files))
@@ -198,22 +247,57 @@ class AdminController extends Controller
      * Displays a form to edit an existing Admin entity.
      *
      */
-    public function editAction(Request $request, Article $article)
+    public function editAction(Request $request, $id)
     {
-        $deleteForm = $this->createDeleteForm($article);
+        $em = $this->getDoctrine()->getManager();
+        $article = $em->getRepository('BLOGBundle:Article')->findOneById($id);
+
         $editForm = $this->createForm('BLOGBundle\Form\ArticleType', $article);
+
         $editForm->handleRequest($request);
 
         if ($editForm->isSubmitted() && $editForm->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
+            $this->getDoctrine()->getManager();
+            var_dump($request->request);
+            die();
+            // On récupères les images de l'article édité
+            foreach($article->getImage() as $image)
+            {
+                // On vérifie si ça ne matche pas avec les infos en bdd
+                // Alors, cela signifie que l'image a été supprimée
+                // Il faudra donc l'enlever de la bdd
+                $i=0;
+                $img = $image;
+                $src= $img->getSrc();
+                // On récupère les src des images envoyées via le formulaire d'édition
+                foreach($request->request->get('src') as $src2)
+                {
+                    if($src2 == $src) // Si le src de l'image envoyée ne correspond pas à
+                    {                // une image en bdd, c'est que l'image en bdd doit être supp
+                        $i++;       // Le compteur est alors égal à 0
+                    }
+                }
+
+                if($i==0)
+                {
+                    var_dump(src2);
+                    // $em->remove($img);
+                }
+             }
+
+
+            $em->flush();
+            echo $i;
+            die();
+
+
 
             return $this->redirectToRoute('admin_edit', array('id' => $article->getId()));
         }
 
         return $this->render('@BLOG/Admin/edit.html.twig', array(
-            'Admin' => $article,
+            'article' => $article,
             'edit_form' => $editForm->createView(),
-            'delete_form' => $deleteForm->createView(),
         ));
     }
 
@@ -221,16 +305,14 @@ class AdminController extends Controller
      * Deletes a Admin entity.
      *
      */
-    public function deleteAction(Request $request, Article $article)
+    public function deleteAction(Request $request, $id)
     {
-        $form = $this->createDeleteForm($article);
-        $form->handleRequest($request);
 
-        if ($form->isSubmitted() && $form->isValid()) {
             $em = $this->getDoctrine()->getManager();
+            $article = $em->getRepository('BLOGBundle:Article')->findOneById($id);
             $em->remove($article);
             $em->flush($article);
-        }
+
 
         return $this->redirectToRoute('admin_index');
     }
